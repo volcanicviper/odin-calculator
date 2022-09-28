fc3fdaa059657d519ec209cf370665f07538adf0
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-09-28 16:20:44 +0200

    Implemented the operate function

--- calc.js
+++ calc.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(op, num1, num2) {
+    switch (op) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
